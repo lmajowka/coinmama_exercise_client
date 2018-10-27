c7ee4143bf7a14e443ed1e302cf01eb0a87d88cc
Leo <leo@Leos-MacBook-Pro.local>
2018-10-27 19:32:36 +0300

    first commit

---
+++ src/Coin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Coin extends Component {
+  render() {
+  	return (
+  		<div>
+	  		<div>Bitcoin</div>
+	  		<div>$6.430</div>
+	  	</div>	
+  	);
+  }
+}
+
+export default Coin;
